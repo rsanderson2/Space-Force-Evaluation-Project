f60372d2daebd43b3124d435d6c5ed482945c84b
Grayson-North <graynorth@att.net>
2022-06-24 19:44:13 -0400

    Added ability to pull list of users

--- SpaceForceEvaluationAppLibrary/Models/UserModel.cs
+++ SpaceForceEvaluationAppLibrary/Models/UserModel.cs
@@ -14,6 +14,10 @@ public class UserModel
     public string rank { get; set; }
     public string teamID { get; set; }
     public string job { get; set; }
+    public DateTime dailyDate { get; set; }
+    public DateTime weeklyDate { get; set; }
+    public DateTime monthlyDate { get; set; }
+
     public List<String> surveyHistory { get; set; }
     public List<String> reportHistory { get; set; }
     public string feedbackTableID { get; set; }
