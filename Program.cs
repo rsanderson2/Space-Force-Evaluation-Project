856b5804dc6734d0a218fa76d3412919c31d47f0
Preston <Prestoncheckan@gmail.com>
2022-07-11 16:37:09 -0400

    More project cleanup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using SpaceForceEvaluations.Data;
 using SpaceForceEvaluations;
 using Microsoft.AspNetCore.Rewrite;
 
