31dbf8e56d194cac6a48311ab999a605c7f40b68
Preston <Prestoncheckan@gmail.com>
2022-07-11 17:09:39 -0400

    Trying unit tests again please dont break

---
+++ TestCases/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Bunit;
+using NUnit.Framework;
+
+namespace TestCases
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
