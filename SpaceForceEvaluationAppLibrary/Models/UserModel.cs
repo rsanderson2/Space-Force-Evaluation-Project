f76aa3e3fff96b44633d226d7fe727cd04351cc5
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-02-24 12:49:52 -0500

    added team management page and notes for updating models

--- SpaceForceEvaluationAppLibrary/Models/UserModel.cs
+++ SpaceForceEvaluationAppLibrary/Models/UserModel.cs
@@ -12,12 +12,16 @@ public class UserModel
     public string email { get; set; }
     public string password { get; set; }
     public string rank { get; set; }
-    public string teamID { get; set; }
+    public string teamID { get; set; } // TODO: Update this because now someone can be in multiple teams
     public string job { get; set; }
     public DateTime dailyDate { get; set; }
     public DateTime weeklyDate { get; set; }
     public DateTime monthlyDate { get; set; }
 
+    // TOOD: add one for list of TeamModels (also make team model)
+
+    // TODO: add a list of users that represent subordinates
+  
     public List<String> surveyHistory { get; set; }
     public List<String> reportHistory { get; set; }
     public string feedbackTableID { get; set; }
