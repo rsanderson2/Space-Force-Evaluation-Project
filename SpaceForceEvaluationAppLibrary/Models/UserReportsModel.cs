d989fb19a82768a90f6907808116430c352506ed
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-03-13 21:36:08 -0400

    hope this works

---
+++ SpaceForceEvaluationAppLibrary/Models/UserReportsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceForceEvaluationAppLibrary.Models;
+
+public class UserReportModel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string userReportID { get; set; }
+    public string category { get; set; }
+    public int score { get; set; }
+}
