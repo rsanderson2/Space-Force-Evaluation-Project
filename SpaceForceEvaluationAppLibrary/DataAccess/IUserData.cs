ffccbe5467edb579e061bc4dafe32853089240c1
Darrell Cole Hill <darrellcolehill@gmail.com>
2023-04-19 13:29:09 -0400

    removed unnecessary methods
    Removed methods that are now moved under the ADCON model

--- SpaceForceEvaluationAppLibrary/DataAccess/IUserData.cs
+++ SpaceForceEvaluationAppLibrary/DataAccess/IUserData.cs
@@ -7,46 +7,25 @@ namespace SpaceForceEvaluationAppLibrary.DataAccess
     public interface IUserData
     {
         Task CreateUser(UserModel user);
+
         Task<UserModel> GetUser(string id);
+
         Task<UserModel> GetUserFromAuthentication(string objectId);
+
         Task<UserModel> GetUserFromFirstName(string firstName);
-
-        Task<UserModel> GetClosestCommander(string userID);
-
-        bool hasDirectADCONOverUser(UserModel potentialSubordinate, UserModel potentialSuperior);
-
-        Task<bool> hasIndirectADCONOverUser(UserModel potentialSubordinate, UserModel potentialSuperior);
 
         Task EmptyAllTeamIDs();
 
         Task<UserModel> GetUserByEmail(String email);
 
-        /*
-        Task<List<UserModel>> GetUsersFromIdList(List<string> userIds);
-        Task<List<UserModel>> GetAllSubordinates(string ObjectIdentifier);
-        Task<UserModel> GetDirectSuperior(string id);
-        Task<UserModel> GetClosestCommander(string id);
-        */
-        Task<bool> ADCONTransfer(String currentUserID, String subordinateID);
-
-        Task<bool> addUserToSelfAssignedEvaluators(String currentUserID, String newEvaluatorID);
-
-        Task<bool> addUserToSuperiorAssignedEvaluators(String currentUserID, String newEvaluatorID);
-
         Task<List<UserModel>> GetUsersAsync();
+
         Task<List<UserModel>> GetUsersFromTeam(string teamId);
+
         Task<List<UserModel>> getAllUsers();
+
         Task UpdateUser(UserModel user);
 
-        Task<List<UserModel>> GetDirectSubordinates(UserModel user);
-
         Task EmptyAllSupordinatesAndSuperiorsList();
-
-        Task removeSelfAssignedEvaluatorFromUser(String evaluatorID, String userID);
-        Task removeSuperiorAssignedEvaluatorFromUser(String evaluatorID, String userID);
-
-        Task EmptyAllEvaluators();
-
-        Task<List<UserModel>> GetUsersByEvaluator(String evaluatorID);
     }
 }
